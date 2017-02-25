9a7c42148142a1f9f7f9cc4719fa7e880dc2d8f9
Tom Butler <tom@r.je>
2017-02-25 18:35:55 +0000

    Ex4

--- ex2.js
+++ ex2.js
@@ -1,18 +1,26 @@
 function drawNumbers() {
-	var tableCells = this.parentNode.getElementsByTagName('td');
+        
+        var table = document.createElement('table');
+        var tr = document.createElement('tr');
+        table.appendChild(tr);
 
-	for (var i = 0; i < tableCells.length; i++) {
-		var randomNumber = Math.ceil(Math.random() * 49);
-		tableCells[i].firstChild.nodeValue = randomNumber;
-	}
+        for (var i = 0; i < 6; i++) {
+                var randomNumber = Math.ceil(Math.random() * 49);
+                var td = document.createElement('td');
+                var textNode = document.createTextNode(randomNumber);
+                td.appendChild(textNode);
+                tr.appendChild(td);             
+        }
+
+        this.parentNode.appendChild(table);
 }
 
 function myLoadEvent() {
-	var buttons = document.getElementsByTagName('button');
+        var buttons = document.getElementsByTagName('button');
 
-	for (var i = 0; i < buttons.length; i++) {
-		buttons[i].addEventListener('click', drawNumbers);
-	}
+        for (var i = 0; i < buttons.length; i++) {
+                buttons[i].addEventListener('click', drawNumbers);
+        }
 }
 
 document.addEventListener('DOMContentLoaded', myLoadEvent);
